4f12ef1e3a8c7d5aa914d10adcaffe180d125959
Jeongmin <skelters@naver.com>
2023-05-24 23:30:26 +0900

    styling

---
+++ src/Components/Todo/Todo.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
+import styles from './Todo.module.css'
+
+export default function Todo({todo, onUpdate, onDelete}) {
+  // todo 반복해서 입력할 필요 없음
+  const {text, status} = todo;
+
+  // 객체 분해할당 부분 참고
+  const handleChange = (e) => {
+    const status = e.target.checked ? 'completed' : 'active';
+
+    // status를 변경해서 넘겨준다.
+    onUpdate({...todo, status});
+  }
+
+  const handleDelete = (e) => onDelete(todo);
+  return (
+    <li className={styles.todo}>
+      <input className={styles.checkbox} type='checkbox' id='checkbox' checked = {status === 'completed'} onChange={handleChange}/>
+      <label htmlFor="checkBox">{text}</label>
+      <button onClick = {handleDelete}><FaTrashAlt /></button>
+    </li>
+  )
+}
